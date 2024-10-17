9fe20289762115a3a0ac4f206c15737723a8af06
Estefania Cervantes <cervantestefania@gmail.com>
2024-10-17 14:43:42 -0600

    views, urls, html

--- start/urls.py
+++ start/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from start.views import vista
+from start.views import (vista, 
+                         inicio,
+                         template1,
+                         template2,
+                         template3
+)
 
 urlpatterns = [
-    path('vista/', vista)
-    
+    path('vista/', vista),
+    path('', inicio),
+    path('deportes/', template1),
+    path('llantas-accesorios/', template2),
+    path('compra/', template3)
 ]
